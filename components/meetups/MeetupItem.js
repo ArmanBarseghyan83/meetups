58e6d9386c67190e72bc62a7da64155b6e653724
Arman <armanbarseghyan83@gmail.com>
2023-10-09 12:30:16 -0700

    MeetupItem() component error fixed and refactored

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -2,22 +2,22 @@ import { useRouter } from "next/router";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
-function MeetupItem(props) {
+function MeetupItem({title, image, address, id}) {
   const router = useRouter();
 
   function showDetailsHandler() {
-    router.push("/" + props.id);
+    router.push(`/${id}`);
   }
 
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <img src={image} alt={title} />
         </div>
         <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
+          <h3>{title}</h3>
+          <address>{address}</address>
         </div>
         <div className={classes.actions}>
           <button onClick={showDetailsHandler}>Show Details</button>
